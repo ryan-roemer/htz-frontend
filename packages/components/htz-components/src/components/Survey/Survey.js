fae004d9c20e57ec658cd94cb04df11bf344d702
yehuda.goldner <goldnery@gmail.com>
2019-02-20 14:23:09 +0200

    fix(survey): make the b tag felacomponent
    affects: @haaretz/htz-components

--- packages/components/htz-components/src/components/Survey/Survey.js
+++ packages/components/htz-components/src/components/Survey/Survey.js
@@ -77,7 +77,7 @@ export default class Survey extends React.Component {
                       />
                       <FelaComponent style={style.legendLabel} as="span">
                         <span>{legend[0]}</span>
-                        <FelaComponent style={style.legendDate} render="span">{legend[1]}</FelaComponent>
+                        <FelaComponent style={style.legendDate} as="span">{legend[1]}</FelaComponent>
                       </FelaComponent>
                     </FelaComponent>
                   );
