d020828d5158d6dd569c877827436341b38bc099
TxHawks <tofu.hawks@gmail.com>
2019-02-21 15:33:29 +0200

    perf(build): transpile internal packages when building apps
    affects: @haaretz/finance, @haaretz/haaretz.co.il, @haaretz/htz-components, @haaretz/htz-theme,
    @haaretz/tm-theme, @haaretz/app-utils, @haaretz/d3-server, @haaretz/dfp, @haaretz/fela-utils,
    @haaretz/graphql-server, @haaretz/htz-css-tools, @haaretz/htz-react-base, @haaretz/htz-react-server,
    @haaretz/htz-user-utils, @haaretz/login, @haaretz/purchase-page
    
    Apps now import the source of packages internal to the monorepo
    and transpile them on build or when running the dev server.
    
    This automatically enables better tree-shaking, and will enable future
    optimizations through transpilation tragets etc. to be set in a single
    place for each app, and on a per-app basis.
    
    This should also greatly improve DX, as changes in the dependant
    packages are watched when running the app's dev server, and there is no
    need to build individual packages on every change. Live reload it then
    also supported out of the box.
    
    Naturally, changes to the server packages such as `htz-react-server` and
    `graphql-server` cannot be hot-reloaded. They still require a build step
    on every change and the app to be restarted.
    
    The basic next configuration have been modularized and moved to
    htz-react-base/base-next-config. The entire base config can be imported
    with `require('@haaretz/htz-react-base/base-next-config`.
    Additionaly, the config's different parts can be imported with
    `require('@haaretz/htz-react-base/base-next-config/<sub-module>`.

---
+++ packages/libs/htz-react-base/base-next-config/bundleAnalyzerConfig.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+const { BUNDLE_ANALYZE, } = process.env;
+const reportsPath = path.join(process.cwd(), 'bundle-report');
+
+module.exports = {
+  analyzeServer: [ 'server', 'both', ].includes(BUNDLE_ANALYZE),
+  analyzeBrowser: [ 'browser', 'both', ].includes(BUNDLE_ANALYZE),
+  bundleAnalyzerConfig: {
+    server: {
+      analyzerMode: 'static',
+      reportFilename: path.join(reportsPath, 'server.html'),
+    },
+    browser: {
+      analyzerMode: 'static',
+      reportFilename: path.join(reportsPath, 'client.html'),
+    },
+  },
+};
--- packages/libs/htz-react-base/scripts/build/index.js
+++ packages/libs/htz-react-base/scripts/build/index.js
@@ -7,61 +7,6 @@ const fs = require('fs');
 const spawn = require('cross-spawn');
 const { checkDir, } = require('./_checkDir');
 
-function buildApp() {
-  const child = spawn('cross-env', [ 'node', require.resolve('./next'), ], {
-    stdio: 'inherit',
-  });
-  child.on('close', code => {
-    if (code === 0) {
-      console.log('Next App successfully built!');
-    }
-  });
-}
-
-function buildLibrary() {
-  const envScripts = {
-    commonjs: require.resolve('./lib'),
-    esm: require.resolve('./esm'),
-    esnext: require.resolve('./esnext'),
-  };
-
-  const { BABEL_ENV, } = process.env;
-  const args = process.argv.slice(2);
-
-  if (BABEL_ENV && !envScripts[BABEL_ENV]) {
-    console.error('The `BABEL_ENV` environment variable must be empty or one of:');
-    console.error(`  ${Object.keys(envScripts).join('\n  ')}`);
-    process.exit(1);
-  }
-
-  Object.keys(envScripts).forEach(env => {
-    if (!BABEL_ENV || env === BABEL_ENV) {
-      const script = envScripts[env];
-      // It would be nice to just `require` the subtasks in this directory in
-      // order to run Babel multiple times, but the Node module cache would
-      // result in the Babel CLI module only being run once (unless we do
-      // something dirty like delete the cache entry). Easier to just spawn
-      // Node multiple times intead.
-      /* eslint-disable no-undef */
-      child = spawn('cross-env', [ 'node', script, ].concat(args), {
-        stdio: 'inherit',
-      });
-
-      child.on('close', code => {
-        if (code !== 0) {
-          console.error(`child process exited with code ${code}`);
-          process.exit(code);
-        }
-        else {
-          process.exitCode = 0;
-        }
-      });
-      /* eslint-enable no-undef */
-      console.log(); // Blank line between different builds.
-    }
-  });
-}
-
 // First, determine if this is a Next.js app or a consumable library.
 // Next apps will just use `next build` (which will run webpack, etc.) while
 // consumable libraries will run Babel on `src`. To avoid requiring developers
@@ -87,9 +32,67 @@ checkDir('pages').then(isDir => {
       else {
         console.error('Could not determine how to build the current package.');
         console.error('* To build a Next.js app, add a `pages` directory.');
-        console.error('* To build a consumable library, add an `src` directory.');
+        console.error(
+          '* To build a consumable library, add an `src` directory.'
+        );
         process.exit(1);
       }
     });
   }
 });
+
+function buildApp() {
+  const child = spawn('cross-env', [ 'node', require.resolve('./next'), ], {
+    stdio: 'inherit',
+  });
+  child.on('close', code => {
+    if (code === 0) {
+      console.log('\nNext App successfully built!');
+    }
+  });
+}
+
+function buildLibrary() {
+  const envScripts = {
+    commonjs: require.resolve('./lib'),
+    esm: require.resolve('./esm'),
+  };
+
+  const { BABEL_ENV, } = process.env;
+  const args = process.argv.slice(2);
+
+  if (BABEL_ENV && !envScripts[BABEL_ENV]) {
+    console.error(
+      'The `BABEL_ENV` environment variable must be empty or one of:'
+    );
+    console.error(`  ${Object.keys(envScripts).join('\n  ')}`);
+    process.exit(1);
+  }
+
+  Object.keys(envScripts).forEach(env => {
+    if (!BABEL_ENV || env === BABEL_ENV) {
+      const script = envScripts[env];
+      // It would be nice to just `require` the subtasks in this directory in
+      // order to run Babel multiple times, but the Node module cache would
+      // result in the Babel CLI module only being run once (unless we do
+      // something dirty like delete the cache entry). Easier to just spawn
+      // Node multiple times instead.
+      /* eslint-disable no-undef */
+      child = spawn('cross-env', [ 'node', script, ].concat(args), {
+        stdio: 'inherit',
+      });
+
+      child.on('close', code => {
+        if (code !== 0) {
+          console.error(`child process exited with code ${code}`);
+          process.exit(code);
+        }
+        else {
+          process.exitCode = 0;
+        }
+      });
+
+      console.log(); // Blank line between different builds.
+    }
+  });
+}
