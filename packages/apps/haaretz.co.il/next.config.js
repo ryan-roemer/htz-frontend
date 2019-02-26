51a71ed6c639d9da2d90c2a5cf93b9c593454075
TxHawks <tofu.hawks@gmail.com>
2019-02-26 14:12:23 +0200

    perf: optimize haaretz.co.il bundle size
    affects: @haaretz/haaretz.co.il, @haaretz/htz-components, @haaretz/htz-react-base

--- packages/apps/haaretz.co.il/next.config.js
+++ packages/apps/haaretz.co.il/next.config.js
@@ -1,5 +1,9 @@
 /* eslint-disable no-param-reassign,import/no-extraneous-dependencies,import/order */
 
+const path = require('path');
+const fs = require('fs');
+const glob = require('glob');
+
 // //////////////////////////// //
 //   Next and Webpack Plugins   //
 // //////////////////////////// //
@@ -23,6 +27,75 @@ const {
   // NODE_ENV,
 } = process.env;
 
+const nextConfig = () => {
+  const { webpack, } = baseNextConfig;
+
+  return {
+    ...baseNextConfig,
+    webpack: (config, opts) => {
+      // if (!opts.isServer) {
+      //   console.log(
+      //     '\n\n\nCONFIG:\n',
+      //     JSON.stringify(config.optimization.minimizer, null, 2)
+      //   );
+      // }
+      // console.log('\n\n\nOPTS:\n', JSON.stringify(opts, null, 2));
+      const {
+        dir,
+        dev,
+        isServer,
+        // buildId,
+        // config: nextConfig,
+        // defaultLoaders,
+        totalPages,
+      } = opts;
+      const isClient = !isServer;
+
+      if (isClient && !dev) {
+        // // Minifier options (uses terser)
+        // config.optimization.minimizer = (
+        //   config.optimization.minimizer || []
+        // ).forEach(item => {
+        //   // extract legal comments to separate file
+        //   item.options.extractComment = true;
+        //
+        //   // don't include any comments in the production file
+        //   if (item.options.terserOptions) item.options.terserOptions.output.comments = false;
+        // });
+
+        // const pagesPath = path.join(dir, 'pages');
+        // const articlePages = (
+        //   glob.sync('**/*article*.js', {
+        //     nonull: false,
+        //     cwd: pagesPath,
+        //     nocase: true,
+        //     nodir: true,
+        //   }) || []
+        // ).length;
+        const { cacheGroups, } = config.optimization.splitChunks;
+
+        // We have many article pages, which share different layouts,
+        // but a fairly common dependency graph. Next's default rule
+        // for including modules in the `commons` package is that a
+        // module needs to be in demand in more that half the pages.
+        // since more than half our pages are articles, non-article pages
+        // are sent a lot of code they will never use.
+        cacheGroups.commons.minChunks = totalPages - 2;
+
+        // cacheGroups.commonVendors = {
+        //   name: 'commons',
+        //   chunks: 'all',
+        //   test: /[\\/]node_modules[\\/]((*.fela.*)|(*.apollo.*)|graphql|next)/,
+        // };
+      }
+
+      return {
+        ...webpack(config, opts),
+      };
+    },
+  };
+};
+
 /**
  * NOTE: Next.js builds the output directory in a temporary location before
  * replacing it at $PWD/.next. If you are referencing the .next directory
@@ -33,5 +106,5 @@ const {
 // ////////////////////////////
 
 module.exports = BUNDLE_ANALYZE
-  ? withBundleAnalyzer(withTranspiledModules(baseNextConfig))
-  : withSourceMaps(withTranspiledModules(baseNextConfig));
+  ? withBundleAnalyzer(withTranspiledModules(nextConfig()))
+  : withSourceMaps(withTranspiledModules(nextConfig()));
