2d282c71a2c8971a486b2c72450e9d4fe8470084
gratzker <gratzker@gmail.com>
2019-02-28 15:32:19 +0200

    feat(components): only non-paying users will be redirected if adBlock in on
    affects: @haaretz/htz-components

--- packages/components/htz-components/src/components/AdBlock/AdBlockRedirect.js
+++ packages/components/htz-components/src/components/AdBlock/AdBlockRedirect.js
@@ -1,19 +1,19 @@
 /* global window */
 // @flow
 import React from 'react';
-// import gql from 'graphql-tag';
+import gql from 'graphql-tag';
 import type { ElementRef, } from 'react';
-// import type { DocumentNode, } from 'graphql/language/ast';
+import type { DocumentNode, } from 'graphql/language/ast';
 import ApolloConsumer from '../ApolloBoundary/ApolloConsumer';
 
 
-/* const USER: DocumentNode = gql`
+const USER: DocumentNode = gql`
   query getUser {
     user @client {
       type
     }
   }
-`; */
+`;
 
 type State = {
   hasListener: boolean,
@@ -30,13 +30,12 @@ class AdBlockRedirect extends React.Component<{}, State> {
     return (
       <ApolloConsumer>
         {client => {
-          // Commented out parts are temporary for testing (ignore user type, do redirect for all users including subscribers). Remove all comments once ready for production.
-          /* const userType = client.readQuery({ query: USER, }).user.type; */
-          if (/* userType && userType !== 'paying' && */ !this.state.hasListener) {
+          const userType = client.readQuery({ query: USER, }).user.type;
+          if (userType && userType !== 'paying' && !this.state.hasListener) {
             this.setState({ hasListener: true, });
             window.addEventListener('load', () => {
-              /* const handlerUserType = client.readQuery({ query: USER, }).user.type; */
-              if (/* handlerUserType && handlerUserType !== 'paying' && */ this.ref && this.ref.clientHeight <= 0) {
+              const handlerUserType = client.readQuery({ query: USER, }).user.type;
+              if (handlerUserType && handlerUserType !== 'paying' && this.ref && this.ref.clientHeight <= 0) {
                 window.document.location.href = `/hblocked?returnTo=${encodeURIComponent(
                   window.document.location.href
                 )}`;
