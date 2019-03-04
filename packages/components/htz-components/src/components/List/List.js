fba1ff58855fd508c70f6ef08440fd7594ffa35a
TxHawks <tofu.hawks@gmail.com>
2019-03-04 16:04:07 +0200

    WIP: try remove list duplications completely to see if it has any effect on scroll perf
    affects: @haaretz/htz-components

--- packages/components/htz-components/src/components/List/List.js
+++ packages/components/htz-components/src/components/List/List.js
@@ -2,9 +2,9 @@
 import * as React from 'react';
 
 import type { ListDataType, } from '../../flowTypes/ListDataType';
-import type { TeaserDataType, } from '../../flowTypes/TeaserDataType';
-import { getTeasersInPage, updateTeasersInPage, } from './ListDuplication';
-import ReadingHistoryProvider from '../ReadingHistory/ReadingHistoryProvider';
+// import type { TeaserDataType, } from '../../flowTypes/TeaserDataType';
+// import { getTeasersInPage, updateTeasersInPage, } from './ListDuplication';
+// import ReadingHistoryProvider from '../ReadingHistory/ReadingHistoryProvider';
 import useGetComponent from '../../hooks/GetComponentContext/useGetComponent';
 
 type ListProps = ListDataType & {
@@ -17,87 +17,77 @@ export default function List(props: ListProps) {
   const getListView = useGetComponent();
 
   const { viewProps, ...listData } = props;
-  const { loadPriority, contentId, } = listData;
-  const isSsr = loadPriority === 'ssr';
+  const { /* loadPriority, */ contentId, } = listData;
+  // const isSsr = loadPriority === 'ssr';
 
   const ListInner = getListView(props.view);
 
-  return isSsr ? (
+  return (
     <ListInner
       listData={listData}
       viewProps={viewProps}
-      updateListDuplication={updateTeasersInPage}
       variables={{
         listId: contentId,
-        history: getTeasersInPage(),
+        // history: getTeasersInPage(),
       }}
     />
-  ) : (
-    <ClientSideList
-      getListDuplication={getTeasersInPage}
-      viewProps={viewProps}
-      listData={listData}
-      updateListDuplication={updateTeasersInPage}
-      ListInner={ListInner}
-    />
   );
 }
 
-type ClientSideListProps = {
-  listData: ListDataType,
-  /** A function that updates the list of article links in the page */
-  updateListDuplication: (Array<TeaserDataType>) => void,
-  getListDuplication: () => Array<string>,
-  viewProps: Object,
-  ListInner: React.ComponentType<any>,
-};
-
+// type ClientSideListProps = {
+//   listData: ListDataType,
+//   /** A function that updates the list of article links in the page */
+//   updateListDuplication: ?(Array<TeaserDataType>) => void,
+//   getListDuplication: () => Array<string>,
+//   viewProps: Object,
+//   ListInner: React.ComponentType<any>,
+// };
+//
 // function ClientSideList({
-function ClientSideList({
-  listData,
-  getListDuplication,
-  updateListDuplication,
-  viewProps,
-  ListInner,
-}: ClientSideListProps) {
-  const [
-    listDuplicationIds: Array<?string>,
-    setListDuplicationsIds,
-  ] = React.useState([]);
-  const [
-    listDuplicationIsUpdated: boolean,
-    setListDuplicationIsUpdated,
-  ] = React.useState(false);
-
-  React.useEffect(
-    () => {
-      setListDuplicationsIds(getListDuplication());
-    },
-    // we want this to run just once when the components is first
-    // rendered, which is why the dependencies array is empty
-    []
-  );
-
-  function updateDuplications(items) {
-    if (!listDuplicationIsUpdated) {
-      updateListDuplication(items);
-      setListDuplicationIsUpdated(true);
-    }
-  }
-
-  return (
-    <ReadingHistoryProvider>
-      {readingHistory => (
-        <ListInner
-          listData={listData}
-          viewProps={viewProps}
-          updateListDuplication={updateDuplications}
-          variables={{
-            listId: listData.contentId,
-            history: [ ...readingHistory, ...(listDuplicationIds || []), ],
-          }}
-        />
-      )}
-    </ReadingHistoryProvider>
-  );
-}
+//   listData,
+//   getListDuplication,
+//   updateListDuplication,
+//   viewProps,
+//   ListInner,
+// }: ClientSideListProps) {
+//   const [
+//     listDuplicationIds: Array<?string>,
+//     setListDuplicationsIds,
+//   ] = React.useState([]);
+//   const [
+//     listDuplicationIsUpdated: boolean,
+//     setListDuplicationIsUpdated,
+//   ] = React.useState(false);
+//
+//   React.useEffect(
+//     () => {
+//       setListDuplicationsIds(getListDuplication());
+//     },
+//     // we want this to run just once when the components is first
+//     // rendered, which is why the dependencies array is empty
+//     []
+//   );
+//
+//   function updateDuplications(items) {
+//     if (!listDuplicationIsUpdated) {
+//       updateListDuplication(items);
+//       setListDuplicationIsUpdated(true);
+//     }
+//   }
+//
+//   return (
+//     <ReadingHistoryProvider>
+//       {readingHistory => (
+//         <ListInner
+//           listData={listData}
+//           viewProps={viewProps}
+//           updateListDuplication={updateDuplications}
+//           variables={{
+//             listId: listData.contentId,
+//             history: [ ...readingHistory, ...(listDuplicationIds || []), ],
+//           }}
+//         />
+//       )}
+//     </ReadingHistoryProvider>
+//   );
+// }
