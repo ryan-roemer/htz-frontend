a81a3fe169679bc725e535cfb4347cb91b85b2ea
TxHawks <tofu.hawks@gmail.com>
2019-03-04 12:26:19 +0200

    perf(browser support): decrease bundle size by limiting supported browsers
    affects: @haaretz/haaretz.co.il, @haaretz/htz-react-base

--- packages/libs/htz-react-base/browsers.js
+++ packages/libs/htz-react-base/browsers.js
@@ -5,10 +5,10 @@ module.exports = [
   'firefox >= 60',
   'ios >= 10',
   'safari >= 10',
-  'samsung >= 4',
+  'samsung >= 6.2',
   // // Android browser support raises the size of our
   // // `commons` bundle size by 70KB!
-  'android >= 4.4.2',
+  // 'android >= 67',
   'not dead',
   'current node',
 ];
